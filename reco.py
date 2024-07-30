677be3948166f78cace4c8ea97fe2e29109ed204
Prateek Trivedi <92620578+prateektrivedi21@users.noreply.github.com>
2024-07-31 00:48:47 +0530

    Update reco.py

--- reco.py
+++ reco.py
@@ -281,7 +281,7 @@ if st.button('Recommend'):
                 </div>
                 <div class='scrollable-text' title='{recommended_movie_names[idx]}'>
                  <h5 class='movie-text'>{recommended_movie_names[idx]}<br></h5>
-                 <h4 class='movie-text'>Released on: {datetime.strptime(recommended_movie_release_dates[idx], '%Y-%m-%d').strftime('%b %d, %Y')})</h4>
+                 <h6 class='movie-text'>Released on: {datetime.strptime(recommended_movie_release_dates[idx], '%Y-%m-%d').strftime('%b %d, %Y')}</h6>
                 </div>
                 """,
                 unsafe_allow_html=True
